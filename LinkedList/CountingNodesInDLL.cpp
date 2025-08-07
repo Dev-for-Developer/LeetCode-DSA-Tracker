a182178cb3371ddad68987c448b60862156bfb93
Dev Sharma <devsh8126@gmail.com>
2025-08-07 16:42:54 +0530

    Counting no of nodes in O(1) T.C

--- LinkedList/CountingNodesInDLL.cpp
+++ LinkedList/CountingNodesInDLL.cpp
@@ -12,27 +12,33 @@ struct Node
         next = NULL;
     }
 };
-int count_no_of_nodes(Node* &head)
-{
-    int count = 0;
-    Node* temp = head;
-    while(temp != NULL)
-    {
-        count++;
-        temp = temp -> next;
-    }
-    return count;
-}
+// int count_no_of_nodes(Node* &head)
+// {
+//     if(head == NULL)
+//     {
+//         cout<<"Empty List..."<<endl;
+//         return 0;
+//     }
+//     int count = 0;
+//     Node* temp = head;
+//     while(temp != NULL)
+//     {
+//         count++;
+//         temp = temp -> next;
+//     }
+//     return count;
+// }
 int main()
 {
     Node* head = NULL;
     Node* tail = NULL;
-    int x;
+    int x; int count = 0;
     while(cin>>x && x != -1)
     {
         Node* newNode = new Node(x);
         if(head == NULL)
         {
+            count++;
             head = newNode;
             tail = newNode;
         }
@@ -41,9 +47,10 @@ int main()
             tail -> next = newNode;
             newNode -> prev = tail;
             tail = tail -> next;
+            count++;
         }
     }
-    int count = 0;
-    count = count_no_of_nodes(head);
+    // int count = 0;
+    // count = count_no_of_nodes(head);
     cout<<"Count: "<<count<<endl;
 }
